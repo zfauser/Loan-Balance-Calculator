1a76ae83ff06817bb826f236b618d6e729b0b875
Zach <19974370+Zfauser@users.noreply.github.com>
2024-03-06 09:37:20 -0500

    March 5t

--- main.cpp
+++ main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 
+using namespace std;
+
+float loanAmount = 0;
+float annualInterestRate = 0;
+float monthlyInterestRate = 0;
+int month = 0;
+
+float getLoanAmount() {}
+
+int getLoanTerm() {}
+
+float getAnnualInterestRate() {}
+
+float getMonthlyInterestRate(float annualRate) {}
+
+float getMonthlyPayment() {}
+
+bool canLoanBePaidOff(int amount, float monthlyRate) {}
+
+void calculateLoan() {}
+
+void displayPayments() {}
+
 int main() {
-  std::cout << "Hello World!\n";
+  cout << "Howdy there, partner! I do declare that this loan calculator is the best in the west! Yeehaw!";
+  cout << "I reckon you should enter your loan amount: ";
+  getLoanAmount();
+  cout << "I reckon you should enter your annual interest rate: ";
+  getAnnualInterestRate();
+  getMonthlyInterestRate(annualInterestRate);
+  cout << "I reckon you should enter your monthly payment: ";
+  getMonthlyPayment();
+  if (canLoanBePaidOff(loanAmount, monthlyInterestRate)) {
+    calculateLoan();
+    displayPayments();
+  } else {
+    cout << "I reckon you can't pay off this loan with that payment.";
+  }
 }
